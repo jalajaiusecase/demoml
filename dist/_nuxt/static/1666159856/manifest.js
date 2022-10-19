a29ffbe719e49aff6dfaaa93f9d85227ca35be16
u26y40 <jalaj.agrawal@avl.com>
2022-10-19 12:38:20 +0530

    Deployment new2

--- dist/_nuxt/static/1666159856/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fexplainability","\u002Ftraining-orchestration","\u002Fmodel-testing","\u002Fmodel-monitoring","\u002Fdata-versioning","\u002Ffeature-store","\u002Fexperiment-tracking","\u002Fmodel-serving","\u002F"]})
